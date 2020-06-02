8a0ccb8325c5f9b6a81b5df13d580d8fdc386b29
RB-Codes <rasheedbader8@gmail.com>
2020-06-02 10:34:36 +0300

    debug Gha

--- JS/app.js
+++ JS/app.js
@@ -34,7 +34,7 @@ var pname = [
    'assets/pen.jpg',
    'assets/pet-sweep.jpg',
    'assets/scissors.jpg',
-   'assets/shark',
+   'assets/shark.jpg',
    'assets/sweep.png',
    'assets/usb.gif',
    'assets/water-can.jpg',
@@ -44,6 +44,8 @@ var pname = [
 
    var kliks = 0;
    var kliklim = 25;
+   var ikliks = [];
+   var iviews = [];
    // calling id
    var leftImage = document.getElementById('leftImage');
    var rightImage = document.getElementById('rightImage');
@@ -60,76 +62,45 @@ var pname = [
     }
     // creating news
     for (var i = 0; i < pname.length; i++) {
-      new Item(pname[i],ppath[i])  
-      
+      new Item(pname[i],ppath[i])
     }
-    // console.log(Item.all)
-
-
     // Making Variables
-
-    var leftImage, midImage, rightImage;
+    var leftImage5, midImage5, rightImage5;
     // Render Function 
-
     function renderImages () {
-
-    leftImage = Item.all[randomNumber(0,Item.all.length-1)];
-    midImage = Item.all[randomNumber(0,Item.all.length-1)];
-    rightImage = Item.all[randomNumber(0,Item.all.length-1)];
-    
-    
     // NO REPETITION
-
-
     do{
       var cas1 =randomNumber(0,Item.all.length-1);
       var cas2 =randomNumber(0,Item.all.length-1);
       var cas3 =randomNumber(0,Item.all.length-1);
     } while (cas1 === cas2 || cas2 === cas3 || cas1 === cas3);
-
-    leftImage = Item.all[cas1];
-    midImage = Item.all[cas2];
-    rightImage = Item.all[cas3];
-    // var rePeated = true;
-    // while(rePeated) {
-    //   if (leftImage == midImage) {
-    //     leftImage = Item.all[randomNumber(0,Item.all.length-1)];
-    //     midImage = Item.all[randomNumber(0,Item.all.length-1)];
-    //   } 
-    //   else if (midImage == rightImage) {
-    //     midImage = Item.all[randomNumber(0,Item.all.length-1)];
-    //     rightImage = Item.all[randomNumber(0,Item.all.length-1)];
-    //   }
-    //   else if (leftImage == rightImage) {
-    //     leftImage = Item.all[randomNumber(0,Item.all.length-1)];
-    //     rightImage = Item.all[randomNumber(0,Item.all.length-1)];
-    //   }
-    //   else {
-    //     rePeated = false;
-    //   }
-    // }
-
-    leftImage.src = leftImage.ipath;
-    leftImage.alt = leftImage.iname;
-    leftImage.title =leftImage.iname;
-
-    midImage.src = midImage.ipath;
-    midImage.alt = midImage.iname;
-    midImage.title = midImage.iname;
-
-    rightImage.src = rightImage.ipath;
-    rightImage.alt = rightImage.iname;
-    rightImage.title =rightImage.iname;
-
+    // Giving items names
+    leftImage5 = Item.all[cas1];
+    midImage5 = Item.all[cas2];
+    rightImage5 = Item.all[cas3];
+    // \\\\\\\\\\\\\\
+    leftImage.src = leftImage5.ipath;
+    leftImage.alt = leftImage5.iname;
+    leftImage.title =leftImage5.iname;
+    // \\\\\\\\\\\\\\\\\\
+    midImage.src = midImage5.ipath;
+    midImage.alt = midImage5.iname;
+    midImage.title = midImage5.iname;
+    // \\\\\\\\\\\\\\\\
+    rightImage.src = rightImage5.ipath;
+    rightImage.alt = rightImage5.iname;
+    rightImage.title =rightImage5.iname;
+// console.log(leftImage);
+// console.log(midImage);
   }
   renderImages();
   
 
     function renderResults () {
-      var ulE1 = document.getElementById('finalResult');
+      var ulE1 = document.getElementById('resulty');
       for( var i =0; i<items.all.length; i++) {
         var li = document.createElement('li');
-        li.textContent = `${items.all[i].goatName} has ${items.all[i].kliks}`;
+        li.textContent = `${Items.all[i].goatName} has ${Items.all[i].kliks}`;
         ulE1.append(li);
       }
     }
@@ -137,26 +108,27 @@ var pname = [
 // \\\\\\\\\\\\\\\\\\
 
 imagesSection.addEventListener('click',klikcount);
-
 function klikcount (event) {
- 
+
   if(kliks < kliklim) {
     
     if(event.target.id !== 'imagesSection') {
       kliks++;
       if(event.target.id === 'leftImage') {
-        leftImage.kliks++;
+        leftImage5.ikliks++;
+
       }
       if(event.target.id === 'rightImage') {
-        rightImage.kliks++;
+        rightImage5.ikliks++;
       }
       if(event.target.id === 'midImage') {
-        midImage.kliks++;
+        midImage5.ikliks++;
       }
       renderImages();
     } 
   } else {
     renderResults();
+    // console.log(kliks)
   }
 }
 
@@ -164,7 +136,9 @@ function renderResults (){
   var ulE1 = document.getElementById('resulty');
   for (var i = 0; i < Item.all.length; i++) {
    var li = document.createElement('li');
-   li.textContent = `${Item.all[i].iname} has ${item.all[i].kliks}`
+   ulE1.appendChild(li)
+   li.textContent = `${Item.all[i].iname} has ${Item.all[i].ikliks}`
+  //  console.log(li)
   }
 }
 // Math dude
